7466e8e2f19e416fa1e3aed2bc28dca3c2559dbd
Abdul Amoud <aamou094@uottawa.ca>
2019-05-19 03:47:51 -0400

    Fixed dbname in Initdb script

--- initdb.js
+++ initdb.js
@@ -6,7 +6,7 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://127.0.0.1:27017/";
 MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
   if (err) throw err;
-  var dbo = db.db("FormBuilder");
+  var dbo = db.db("quickApi");
   dbo.createCollection("forms", (err, res) => {
     if (err) throw err
     console.log("forms created!");
